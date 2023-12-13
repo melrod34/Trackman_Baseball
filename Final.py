d2f4c36f12e3fb8935045fa6bb4c54dbc45e1424
melrod34 <mjelrod@me.com>
2023-12-13 15:05:54 -0500

    Update Final.py

--- Final.py
+++ Final.py
@@ -11,7 +11,7 @@ def outfield_wall(x):
     a = 340 
     b = 380  
     c = 340  
-    t = (x + 75) / 150  
+    t = (x + 90) / 180  
     return (1 - t)**2 * a + 2 * (1 - t) * t * b + t**2 * c
 
 def outfield_line(x):
@@ -32,7 +32,7 @@ selected_hitter = st.sidebar.selectbox('Select Hitter:', hitter_options)
 
 
 
-play_result_options = ['All', 'Out', 'FieldersChoice', 'Error', 'Sacrifice', 'Single', 'Double', 'Triple', 'HomeRun']
+play_result_options = ['Out', 'FieldersChoice', 'Error', 'Sacrifice', 'Single', 'Double', 'Triple', 'HomeRun']
 selected_play_result = st.sidebar.selectbox('Select Play Result:', play_result_options)
 
 
@@ -63,7 +63,7 @@ plt.figure(figsize=(20, 15))
 
 scatter = plt.scatter(
     new_df['x_pos'],
-    new_df['y_pos'],
+    new_df['LastTrackedDistance'],
     c=new_df['ExitSpeed'],  
     cmap='Reds',  
     marker='o',
@@ -78,23 +78,23 @@ cbar.ax.tick_params(labelsize=16)
 plt.title(f'{selected_hitter} - Batter Spray Chart', fontsize = 40)
 
 # Foul Lines
-plt.plot([-75, 0, 75], [340, 0, 340], color='green', linestyle='--', linewidth=4)  
+plt.plot([-90, 0, 90], [340, 0, 340], color='green', linestyle='--', linewidth=4)  
  
 
 # Wall
-x_curve = np.linspace(-75, 75, 100)
+x_curve = np.linspace(-90, 90, 100)
 y_curve = outfield_wall(x_curve)
 plt.plot(x_curve, y_curve, color='red', linewidth=4)
 
 # Outfield Line
-x_starting_line = np.linspace(-21, 21, 100)
+x_starting_line = np.linspace(-24, 24, 110)
 y_starting_line = outfield_line(x_starting_line)
 plt.plot(x_starting_line, y_starting_line, color='orange', linestyle='-', linewidth=4)
 
 # Bases
 plt.scatter([0], [127], color='red', marker='s', s=75, zorder=10)
-plt.scatter([-19], [90], color='red', marker='s', s=75, zorder=10)
-plt.scatter([19], [90], color='red', marker='s', s=75, zorder=10)
+plt.scatter([-24], [90], color='red', marker='s', s=75, zorder=10)
+plt.scatter([24], [90], color='red', marker='s', s=75, zorder=10)
 plt.scatter([0], [0], color='red', marker='s', s=75, zorder=10)
 
 plt.xlim(-105, 105)
